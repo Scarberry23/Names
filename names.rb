d13e99179e45ced46c457c51b5fc642a91c2b26d
brandon <bscarberry23@yahoo.com>
2017-06-26 11:41:22 -0400

    my name is

--- names.rb
+++ names.rb
@@ -1,5 +1,16 @@
-def firstname
-	print "brandon"
+def mynameis
+	print "My name is"
 end
 
+def firstname 
+	print " Brandon "
+end
+
+def lastname
+	print " Scarberry "
+end
+
+
+mynameis
 firstname
+lastname
